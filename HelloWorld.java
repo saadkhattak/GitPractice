11c4c524c0c5e918e52ca9b604886bf3192b43f5
ABC <abc@gmail.com>
2019-05-28 16:53:37 +0000

    Changed the display message of Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         //This commond will print Hello, World!
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello World...I Love this java program!"); 
     }
 }
